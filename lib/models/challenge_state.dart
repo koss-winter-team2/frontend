afb67d0c938a138bad3c66997ab671db5ce925f4
sungyeon6 <0504sypark@naver.com>
2026-02-07 02:47:47 +0900

    [FEAT] 챌린지 인증 화면

---
+++ lib/models/challenge_state.dart
@@ -0,0 +1,18 @@
+class ChallengeState {
+  final int photoCount;
+
+  const ChallengeState({
+    required this.photoCount,
+  });
+
+  bool get isCompleted => photoCount >= 3;
+
+
+  ChallengeState copyWith({
+    int? photoCount,
+  }) {
+    return ChallengeState(
+      photoCount: photoCount ?? this.photoCount,
+    );
+  }
+}
